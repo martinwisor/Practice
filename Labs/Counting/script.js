b2ac735591da5e862e910bd84e1d47f7e2e8d4dc
Martin Wisor <martinwisor@Martins-iMac.lan>
2023-04-29 13:56:28 -0400

    counting

--- Labs/Counting/script.js
+++ Labs/Counting/script.js
@@ -1,10 +1,50 @@
+// for loop
 for ( let i = 1; i < 11; i++){
     console.log(i)
 }
 for (let i = 10; i>0; i--){
     console.log(i)
 }
-let count = 1;
-while (count < 11){
-console.log(count);
-count++}
+// while loop
+let i = 1;
+while (i < 11){
+console.log(i);
+i++}
+
+let j = 10;
+while (j > 0){
+    console.log(j);
+    j--
+}
+
+// do while loop
+let k = 1;
+do{
+    console.log(k)
+    k++;
+}
+while (k<11)
+
+let l = 10;
+do{
+    console.log(l)
+    l--
+}
+while(l>0)
+
+// for of loop
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (const element of numbers){
+    console.log(element)
+}
+
+// padStart
+let str = "console";
+console.log(str.padStart(10));
+
+let str1 = "house";
+console.log(str1.padStart(10));
+
+let str2 = "car";
+console.log(str2.padStart(10));
