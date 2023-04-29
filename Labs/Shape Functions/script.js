1be931c1297a8606cf8eaa8bd339c31c32070a00
Martin Wisor <martinwisor@Martins-iMac.lan>
2023-04-29 14:20:50 -0400

    shape functions

---
+++ Labs/Shape Functions/script.js
@@ -0,0 +1,23 @@
+function getAreaOfCircle(radius){
+    return 3.14*radius**2
+}
+console.log(getAreaOfCircle(50))
+// 7850
+
+function getCircumferenceOfCircle(radius){
+    return 2*3.14*radius
+}
+console.log(getCircumferenceOfCircle(50))
+// 314
+
+function getAreaofSquare(side){
+    return side**2
+}
+console.log(getAreaofSquare(50))
+// 2500
+
+function getAreaOfTriangle(base, height){
+    return (base*height) / 2
+}
+console.log(getAreaOfTriangle(50, 50))
+// 1250
